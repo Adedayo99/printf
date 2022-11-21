cafc0acfd69190bed7528479a133d7ed192f3f1c
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-21 04:11:59 -0800

    main.h

--- main.h
+++ main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+typedef struct op {
+	char op;
+	int (*f)(char *);
+
+	} op_t;
+
+
+
 int _printf(const char *format, ...);
 int printf_1(int *c);
 int printf_2(const char *c);
