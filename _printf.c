26f4443b701bda4dbfa58757a3744392d58abe5b
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-22 04:13:13 -0800

    All bugs resolved

--- _printf.c
+++ _printf.c
@@ -7,19 +7,17 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include "main.h"
-
+#include <string.h>
 
 int _printf(char *format, ...)
 {
-	int x, j;
+	int x;
 	int i = 0;
 	char  *ch;
 	int return_val = 0;
 	int (*func)(char *);
 	int (*func1)(int);
 	va_list args;
-	char check[] = {"csbdi"};
-	int exit_flag = 0;
 	va_start(args, format);
 
 	while (format[i] != '\0')
@@ -27,43 +25,34 @@ int _printf(char *format, ...)
 
 		if (format[i] != '%')
 		{
-			if (i > 0 && format[i - 1] == '%')
-			{
-				for (j = 0; check[j] != '\0'; j++)
-				{	if (format[i] == check[j])
-					exit_flag = 1;
-				}
-			}
-
-			if (exit_flag == 1)
-			break;
 			write(1, &format[i], 1);
 			return_val += 1;
 		}
 
-		else if (format[i] == '%' && format[i + 1] == 'c')
+		if (format[i] == '%' && format[i + 1] == 'c')
 		{
 			x = va_arg(args, int);
 			func = action_func('c');
 			return_val += (*func)((char *) &x);
-
+			i++;
 		}
 
-		else if (format[i] == '%' && format[i + 1] == 's')
+		if (format[i] == '%' && format[i + 1] == 's')
 		{	
 			ch = va_arg(args, char *);
 			func = action_func('s');
 			return_val += (*func)(ch);
-	
+			i++;
 		}
 
-		else if (format[i] == '%' && format[i + 1] == '%')
+		if (format[i] == '%' && format[i + 1] == '%')
 		{
 			write(1, "%", 1);
 			return_val += 1;
+			i++;
 		}
 
-		else if ((format[i] == '%' && format[i + 1] == 'd') 
+		if ((format[i] == '%' && format[i + 1] == 'd') 
 			|| (format[i] == '%' && format[i + 1] == 'i'))
 		{
 			char str[20];
@@ -72,22 +61,21 @@ int _printf(char *format, ...)
 			sprintf(str, "%d", x);
 			func = action_func('d');
 			return_val += (*func)(str);
-		}			
+			i++;
+		}
+			
 
-		else if (format[i] == '%' && format[i + 1] == 'b')
+		if (format[i] == '%' && format[i + 1] == 'b')
 		{
 			x = va_arg(args, int);
 			func1 = action_func1('b');
 			return_val += (*func1)(x);
+			i++;
 		}
 
 
 
 
-		else
-		{
-			exit(98);
-		}
 		i++;
 	}
 
